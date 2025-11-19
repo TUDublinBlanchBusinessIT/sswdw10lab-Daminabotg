11d55f729d49593555d70523a0d6aad2e46298f8
B00161001 Daminabo Tom-George <B00161001@mytudublin.ie>
2025-11-19 10:34:18 +0000

    Completed Part 1

--- confirm.php
+++ confirm.php
@@ -1,35 +1,37 @@
 <?php
 session_start();
+
+
 if (!isset($_POST['confirm'])) {
     header("Location: flightBooking.html");
+    exit();
 }
+
 $fn = $_SESSION['passengerFN'];
 $sn = $_SESSION['passengerSN'];
-if ($_SESSION['luggage']=='1') {
+
+if ($_SESSION['luggage'] == '1') {
     $subTenKG = $_SESSION['subTenKG'];
     $overTenKG = $_SESSION['overTenKG'];
-}
-else {
+} else {
     $subTenKG = 0;
     $overTenKG = 0;
 }
-    
+
 include("dbcon.php");
-$sql = "insert into flightbooking(firstname,surname,bagsUnderTenKG,bagsOverTenKG) values ";
 
-$sql .= "('$fn','$sn','1','1')";
 
-//echo $sql;
-
+$sql = "INSERT INTO flightbooking (firstname, surname, bagsUnderTenKG, bagsOverTenKG) 
+        VALUES ('$fn', '$sn', $subTenKG, $overTenKG)";
 
 if (mysqli_query($conn, $sql)) {
-  echo "Your flight booking was created successfully";
+    echo "Your flight booking was created successfully";
 } else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
 
-session_destroy();
 
+session_destroy();
 ?>
